930b9defdf18f3470d6110a7cb4c5860892a53ea
herpichfr <fabiorafaelh@gmail.com>
2023-08-08 09:55:32 +0100

    Update keybindings.py

--- quake/keybindings.py
+++ quake/keybindings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8; -*-
 """
-Copyright (C) 2007-2013 Guake authors
+Copyright (C) 2007-2013 Quake authors
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
@@ -17,6 +17,12 @@ License along with this program; if not, write to the
 Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 Boston, MA 02110-1301 USA
 """
+from quake.split_utils import SplitMover
+from quake.split_utils import FocusMover
+from quake.common import pixmapfile
+from quake import notifier
+from gi.repository import Gtk
+from gi.repository import Gdk
 import logging
 
 from collections import defaultdict
@@ -24,13 +30,7 @@ from collections import defaultdict
 import gi
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gdk
-from gi.repository import Gtk
 
-from guake import notifier
-from guake.common import pixmapfile
-from guake.split_utils import FocusMover
-from guake.split_utils import SplitMover
 
 log = logging.getLogger(__name__)
 
@@ -39,11 +39,11 @@ class Keybindings:
 
     """Handles changes in keyboard shortcuts."""
 
-    def __init__(self, guake):
-        """Constructor of Keyboard, only receives the guake instance
+    def __init__(self, quake):
+        """Constructor of Keyboard, only receives the quake instance
         to be used in internal methods.
         """
-        self.guake = guake
+        self.quake = quake
         self.accel_group = None  # see reload_accelerators
         self._lookup = None
         self._masks = None
@@ -53,58 +53,61 @@ class Keybindings:
         self.globalhotkeys = {}
         globalkeys = ["show-hide", "show-focus"]
         for key in globalkeys:
-            guake.settings.keybindingsGlobal.onChangedValue(key, self.reload_global)
-            guake.settings.keybindingsGlobal.triggerOnChangedValue(
-                guake.settings.keybindingsGlobal, key, None
+            quake.settings.keybindingsGlobal.onChangedValue(
+                key, self.reload_global)
+            quake.settings.keybindingsGlobal.triggerOnChangedValue(
+                quake.settings.keybindingsGlobal, key, None
             )
 
         def x(*args):
-            prompt_cfg = self.guake.settings.general.get_int("prompt-on-close-tab")
-            self.guake.get_notebook().delete_page_current(prompt=prompt_cfg)
+            prompt_cfg = self.quake.settings.general.get_int(
+                "prompt-on-close-tab")
+            self.quake.get_notebook().delete_page_current(prompt=prompt_cfg)
 
         # Setup local keys
         self.keys = [
-            ("toggle-fullscreen", self.guake.accel_toggle_fullscreen),
-            ("new-tab", self.guake.accel_add),
-            ("new-tab-home", self.guake.accel_add_home),
-            ("new-tab-cwd", self.guake.accel_add_cwd),
+            ("toggle-fullscreen", self.quake.accel_toggle_fullscreen),
+            ("new-tab", self.quake.accel_add),
+            ("new-tab-home", self.quake.accel_add_home),
+            ("new-tab-cwd", self.quake.accel_add_cwd),
             ("close-tab", x),
-            ("rename-current-tab", self.guake.accel_rename_current_tab),
-            ("previous-tab", self.guake.accel_prev),
-            ("previous-tab-alt", self.guake.accel_prev),
-            ("next-tab", self.guake.accel_next),
-            ("next-tab-alt", self.guake.accel_next),
-            ("clipboard-copy", self.guake.accel_copy_clipboard),
-            ("clipboard-paste", self.guake.accel_paste_clipboard),
-            ("select-all", self.guake.accel_select_all),
-            ("quit", self.guake.accel_quit),
-            ("zoom-in", self.guake.accel_zoom_in),
-            ("zoom-in-alt", self.guake.accel_zoom_in),
-            ("zoom-out", self.guake.accel_zoom_out),
-            ("increase-height", self.guake.accel_increase_height),
-            ("decrease-height", self.guake.accel_decrease_height),
-            ("increase-transparency", self.guake.accel_increase_transparency),
-            ("decrease-transparency", self.guake.accel_decrease_transparency),
-            ("toggle-transparency", self.guake.accel_toggle_transparency),
-            ("search-on-web", self.guake.search_on_web),
-            ("open-link-under-terminal-cursor", self.guake.open_link_under_terminal_cursor),
-            ("move-tab-left", self.guake.accel_move_tab_left),
-            ("move-tab-right", self.guake.accel_move_tab_right),
-            ("switch-tab1", self.guake.gen_accel_switch_tabN(0)),
-            ("switch-tab2", self.guake.gen_accel_switch_tabN(1)),
-            ("switch-tab3", self.guake.gen_accel_switch_tabN(2)),
-            ("switch-tab4", self.guake.gen_accel_switch_tabN(3)),
-            ("switch-tab5", self.guake.gen_accel_switch_tabN(4)),
-            ("switch-tab6", self.guake.gen_accel_switch_tabN(5)),
-            ("switch-tab7", self.guake.gen_accel_switch_tabN(6)),
-            ("switch-tab8", self.guake.gen_accel_switch_tabN(7)),
-            ("switch-tab9", self.guake.gen_accel_switch_tabN(8)),
-            ("switch-tab10", self.guake.gen_accel_switch_tabN(9)),
-            ("switch-tab-last", self.guake.accel_switch_tab_last),
-            ("reset-terminal", self.guake.accel_reset_terminal),
+            ("rename-current-tab", self.quake.accel_rename_current_tab),
+            ("previous-tab", self.quake.accel_prev),
+            ("previous-tab-alt", self.quake.accel_prev),
+            ("next-tab", self.quake.accel_next),
+            ("next-tab-alt", self.quake.accel_next),
+            ("clipboard-copy", self.quake.accel_copy_clipboard),
+            ("clipboard-paste", self.quake.accel_paste_clipboard),
+            ("select-all", self.quake.accel_select_all),
+            ("quit", self.quake.accel_quit),
+            ("zoom-in", self.quake.accel_zoom_in),
+            ("zoom-in-alt", self.quake.accel_zoom_in),
+            ("zoom-out", self.quake.accel_zoom_out),
+            ("increase-height", self.quake.accel_increase_height),
+            ("decrease-height", self.quake.accel_decrease_height),
+            ("increase-transparency", self.quake.accel_increase_transparency),
+            ("decrease-transparency", self.quake.accel_decrease_transparency),
+            ("toggle-transparency", self.quake.accel_toggle_transparency),
+            ("search-on-web", self.quake.search_on_web),
+            ("open-link-under-terminal-cursor",
+             self.quake.open_link_under_terminal_cursor),
+            ("move-tab-left", self.quake.accel_move_tab_left),
+            ("move-tab-right", self.quake.accel_move_tab_right),
+            ("switch-tab1", self.quake.gen_accel_switch_tabN(0)),
+            ("switch-tab2", self.quake.gen_accel_switch_tabN(1)),
+            ("switch-tab3", self.quake.gen_accel_switch_tabN(2)),
+            ("switch-tab4", self.quake.gen_accel_switch_tabN(3)),
+            ("switch-tab5", self.quake.gen_accel_switch_tabN(4)),
+            ("switch-tab6", self.quake.gen_accel_switch_tabN(5)),
+            ("switch-tab7", self.quake.gen_accel_switch_tabN(6)),
+            ("switch-tab8", self.quake.gen_accel_switch_tabN(7)),
+            ("switch-tab9", self.quake.gen_accel_switch_tabN(8)),
+            ("switch-tab10", self.quake.gen_accel_switch_tabN(9)),
+            ("switch-tab-last", self.quake.accel_switch_tab_last),
+            ("reset-terminal", self.quake.accel_reset_terminal),
             (
                 "split-tab-vertical",
-                lambda *args: self.guake.get_notebook()
+                lambda *args: self.quake.get_notebook()
                 .get_current_terminal()
                 .get_parent()
                 .split_v()
@@ -112,7 +115,7 @@ class Keybindings:
             ),
             (
                 "split-tab-horizontal",
-                lambda *args: self.guake.get_notebook()
+                lambda *args: self.quake.get_notebook()
                 .get_current_terminal()
                 .get_parent()
                 .split_h()
@@ -120,13 +123,13 @@ class Keybindings:
             ),
             (
                 "close-terminal",
-                lambda *args: self.guake.get_notebook().get_current_terminal().kill() or True,
+                lambda *args: self.quake.get_notebook().get_current_terminal().kill() or True,
             ),
             (
                 "focus-terminal-up",
                 (
-                    lambda *args: FocusMover(self.guake.window).move_up(
-                        self.guake.get_notebook().get_current_terminal()
+                    lambda *args: FocusMover(self.quake.window).move_up(
+                        self.quake.get_notebook().get_current_terminal()
                     )
                     or True
                 ),
@@ -134,8 +137,8 @@ class Keybindings:
             (
                 "focus-terminal-down",
                 (
-                    lambda *args: FocusMover(self.guake.window).move_down(
-                        self.guake.get_notebook().get_current_terminal()
+                    lambda *args: FocusMover(self.quake.window).move_down(
+                        self.quake.get_notebook().get_current_terminal()
                     )
                     or True
                 ),
@@ -143,8 +146,8 @@ class Keybindings:
             (
                 "focus-terminal-right",
                 (
-                    lambda *args: FocusMover(self.guake.window).move_right(
-                        self.guake.get_notebook().get_current_terminal()
+                    lambda *args: FocusMover(self.quake.window).move_right(
+                        self.quake.get_notebook().get_current_terminal()
                     )
                     or True
                 ),
@@ -152,8 +155,8 @@ class Keybindings:
             (
                 "focus-terminal-left",
                 (
-                    lambda *args: FocusMover(self.guake.window).move_left(
-                        self.guake.get_notebook().get_current_terminal()
+                    lambda *args: FocusMover(self.quake.window).move_left(
+                        self.quake.get_notebook().get_current_terminal()
                     )
                     or True
                 ),
@@ -162,7 +165,7 @@ class Keybindings:
                 "move-terminal-split-up",
                 (
                     lambda *args: SplitMover.move_up(  # keep make style from concat this lines
-                        self.guake.get_notebook().get_current_terminal()
+                        self.quake.get_notebook().get_current_terminal()
                     )
                     or True
                 ),
@@ -171,7 +174,7 @@ class Keybindings:
                 "move-terminal-split-down",
                 (
                     lambda *args: SplitMover.move_down(  # keep make style from concat this lines
-                        self.guake.get_notebook().get_current_terminal()
+                        self.quake.get_notebook().get_current_terminal()
                     )
                     or True
                 ),
@@ -180,7 +183,7 @@ class Keybindings:
                 "move-terminal-split-left",
                 (
                     lambda *args: SplitMover.move_left(  # keep make style from concat this lines
-                        self.guake.get_notebook().get_current_terminal()
+                        self.quake.get_notebook().get_current_terminal()
                     )
                     or True
                 ),
@@ -189,16 +192,17 @@ class Keybindings:
                 "move-terminal-split-right",
                 (
                     lambda *args: SplitMover.move_right(  # keep make style from concat this lines
-                        self.guake.get_notebook().get_current_terminal()
+                        self.quake.get_notebook().get_current_terminal()
                     )
                     or True
                 ),
             ),
-            ("search-terminal", self.guake.accel_search_terminal),
-            ("toggle-hide-on-lose-focus", self.guake.accel_toggle_hide_on_lose_focus),
+            ("search-terminal", self.quake.accel_search_terminal),
+            ("toggle-hide-on-lose-focus", self.quake.accel_toggle_hide_on_lose_focus),
         ]
         for key, _ in self.keys:
-            guake.settings.keybindingsLocal.onChangedValue(key, self.reload_accelerators)
+            quake.settings.keybindingsLocal.onChangedValue(
+                key, self.reload_accelerators)
         self.reload_accelerators()
 
     def reload_global(self, settings, key, user_data):
@@ -207,28 +211,28 @@ class Keybindings:
             return
 
         try:
-            self.guake.hotkeys.unbind(self.globalhotkeys[key])
+            self.quake.hotkeys.unbind(self.globalhotkeys[key])
         except BaseException:
             pass
 
         self.globalhotkeys[key] = value
         if key == "show-hide":
             log.debug("reload_global: %r", value)
-            if not self.guake.hotkeys.bind(value, self.guake.show_hide):
+            if not self.quake.hotkeys.bind(value, self.quake.show_hide):
                 keyval, mask = Gtk.accelerator_parse(value)
                 label = Gtk.accelerator_get_label(keyval, mask)
-                filename = pixmapfile("guake-notification.png")
+                filename = pixmapfile("quake-notification.png")
                 notifier.showMessage(
-                    _("Guake Terminal"),
+                    _("Quake Terminal"),
                     _(
                         "A problem happened when binding <b>%s</b> key.\n"
-                        "Please use Guake Preferences dialog to choose another "
+                        "Please use Quake Preferences dialog to choose another "
                         "key"
                     )
                     % label,
                     filename,
                 )
-        elif key == "show-focus" and not self.guake.hotkeys.bind(value, self.guake.show_focus):
+        elif key == "show-focus" and not self.quake.hotkeys.bind(value, self.quake.show_focus):
             log.warning("can't bind show-focus key")
 
     def activate(self, window, event):
@@ -266,23 +270,23 @@ class Keybindings:
         return False
 
     def reload_accelerators(self, *args):
-        """Reassign an accel_group to guake main window and guake
+        """Reassign an accel_group to quake main window and quake
         context menu and calls the load_accelerators method.
         """
         self._lookup = defaultdict(dict)
         self._masks = 0
 
         self.load_accelerators()
-        self.guake.accel_group = self
+        self.quake.accel_group = self
 
     def load_accelerators(self):
-        """Reads all gconf paths under /org/guake/keybindings/local
+        """Reads all gconf paths under /org/quake/keybindings/local
         and adds to the _lookup.
         """
 
         for binding, action in self.keys:
             key, mask = Gtk.accelerator_parse(
-                self.guake.settings.keybindingsLocal.get_string(binding)
+                self.quake.settings.keybindingsLocal.get_string(binding)
             )
             if key > 0:
                 self._lookup[mask][key] = action
