6e601f63b57ef0ba5f49610720f903e40abc99f9
herpichfr <fabiorafaelh@gmail.com>
2023-08-08 12:12:38 +0100

    Opdate test_about.py
    - Update test_about.py
    - Rename all png's for desktop interaction

--- quake/tests/test_about.py
+++ quake/tests/test_about.py
@@ -3,20 +3,20 @@
 
 import pytest
 
-from guake import guake_version
-from guake.about import AboutDialog
+from quake import quake_version
+from quake.about import AboutDialog
 
 
 @pytest.fixture
 def dialog(mocker, monkeypatch):
-    mocker.patch("guake.simplegladeapp.Gtk.Widget.show_all")
+    mocker.patch("quake.simplegladeapp.Gtk.Widget.show_all")
     monkeypatch.setenv("LANGUAGE", "en_US.UTF-8")
     yield AboutDialog()
 
 
 def test_version_test(dialog):
-    assert dialog.get_widget("aboutdialog").get_version() == guake_version()
+    assert dialog.get_widget("aboutdialog").get_version() == quake_version()
 
 
 def test_title(dialog):
-    assert dialog.get_widget("aboutdialog").get_title() == "About Guake"
+    assert dialog.get_widget("aboutdialog").get_title() == "About Quake"
